e685f24bd9243ca8d44f60014c2ab7db398a2013
Adam <ad-91@live.co.uk>
2024-09-15 22:10:49 +0100

    cleaned up by removing unnused function

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -75,31 +75,6 @@ function show_prompt -d "Shows prompt with cue for current priv"
   set_color normal
 end
 
-function get_git_status -d "Gets the current git status"
-  if command git rev-parse --is-inside-work-tree >/dev/null 2>&1
-    set -l dirty (command git status -s --ignore-submodules=dirty | wc -l | sed -e 's/^ *//' -e 's/ *$//' 2> /dev/null)
-    set -l ref (command git describe --tags --exact-match 2> /dev/null ; or command git symbolic-ref --short HEAD 2> /dev/null ; or command git rev-parse --short HEAD 2> /dev/null)
-
-    if [ "$dirty" != "0" ]
-      set_color -b normal
-      set_color red
-      echo "$dirty^"
-      # if [ "$dirty" != "1" ]
-      #   echo "s"
-      # end
-      # echo " "
-      set_color -b red
-      set_color white
-    else
-      set_color -b brgreen
-      set_color white
-    end
-
-    echo "$ref"
-    set_color normal
-   end
-end
-
 function get_git_branch -d "Gets the current git branch"
   if test -d .git
     # Get the current branch
@@ -123,7 +98,6 @@ function fish_prompt
   show_virtualenv
   show_user
   show_pwd
-  # get_git_status
   get_git_branch
   show_prompt
 end
